633d0f54b8c54ea3d3e5e8551954e2816a641c07
kopperp <paulus.j.kopperoinen@student.jyu.fi>
2025-11-25 13:27:40 +0200

    en tiedä mitä kommitoin

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 def main():
     while True:
         print("Tama on kayttoliittymasovellus viitteiden hallintaan bibtex muodossa \nMahdolliset komennot: \n New: Lisaa uusi viite \n")
-        response = input("laita quit \n")
+        response = input("laita quit \n").strip().lower()  
         print(response + "\n")
         if response == "quit":
             break
